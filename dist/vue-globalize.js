15a6c5b06c63ffbc9c327db77a5ff691aa8dd47f
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-05-11 01:05:01 +0200

    version 0.1.1
    update dependencies, fix eslint config

--- dist/vue-globalize.js
+++ dist/vue-globalize.js
@@ -1,5 +1,5 @@
 /*!
- * vue-globalize v0.1.0
+ * vue-globalize v0.1.1
  * (c) 2016 Nikola Kovacs
  * Released under the MIT License.
  */
@@ -61,11 +61,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })();
-
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
+
+	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 	var _util = __webpack_require__(1);
 
@@ -78,9 +78,9 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	// import Globalize from 'globalize';
 
-	var Vue = undefined;
+	var Vue = void 0;
 	var installed = false;
-	var globalizeLoader = undefined;
+	var globalizeLoader = void 0;
 	var defaultCategories = [];
 
 	function wrapGlobalize(globalize) {
@@ -199,31 +199,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	                            _this.$set('globalize', globalize);
 
 	                            var newCategoriesLoaded = {};
-	                            var _iteratorNormalCompletion = true;
-	                            var _didIteratorError = false;
-	                            var _iteratorError = undefined;
-
-	                            try {
-	                                for (var _iterator = _this.categoriesInUse[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	                                    var category = _step.value;
-
-	                                    newCategoriesLoaded[category] = true;
-	                                }
-	                            } catch (err) {
-	                                _didIteratorError = true;
-	                                _iteratorError = err;
-	                            } finally {
-	                                try {
-	                                    if (!_iteratorNormalCompletion && _iterator.return) {
-	                                        _iterator.return();
-	                                    }
-	                                } finally {
-	                                    if (_didIteratorError) {
-	                                        throw _iteratorError;
-	                                    }
-	                                }
+	                            for (var i = 0, l = _this.categoriesInUse.length; i < l; i++) {
+	                                var category = _this.categoriesInUse[i];
+	                                newCategoriesLoaded[category] = true;
 	                            }
-
 	                            vm.categoriesLoaded = newCategoriesLoaded;
 
 	                            resolve({
@@ -405,34 +384,13 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	    var fail = false;
 	    var fns = ['loadGlobalize'];
-	    var _iteratorNormalCompletion2 = true;
-	    var _didIteratorError2 = false;
-	    var _iteratorError2 = undefined;
-
-	    try {
-	        for (var _iterator2 = fns[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-	            var fn = _step2.value;
-
-	            if (typeof options[fn] !== 'function') {
-	                (0, _util.warn)('missing "' + fn + '" function');
-	                fail = true;
-	            }
-	        }
-	    } catch (err) {
-	        _didIteratorError2 = true;
-	        _iteratorError2 = err;
-	    } finally {
-	        try {
-	            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-	                _iterator2.return();
-	            }
-	        } finally {
-	            if (_didIteratorError2) {
-	                throw _iteratorError2;
-	            }
+	    for (var i = 0, l = fns.length; i < l; i++) {
+	        var fn = fns[i];
+	        if (typeof options[fn] !== 'function') {
+	            (0, _util.warn)('missing "' + fn + '" function');
+	            fail = true;
 	        }
 	    }
-
 	    if (fail) {
 	        return;
 	    }
